cc4157ada8932192dae7d77cc5fab235cad0f1af
kevinliebergen <kevinliebergen@hotmail.com>
2021-09-23 16:22:26 +0000

    minor changes

---
+++ readme.md
@@ -0,0 +1,8 @@
+1. Change to api\_telegram.json
+2. Fill correct fields
+3. Create a virtual enviroment:
+ - `$ virtualenv venv; source venv/bin/activate`
+4. Install dependencies:
+ - `$ pip3 install -r requirements`
+5. Execute it in the cron
+
--- recordar.py
+++ recordar.py
@@ -1,10 +1,13 @@
-import time
 import telebot
+import time
 import json
+import os
+
+
 
 class Telegram:
-    def __init__(self):
-        with open('api_telegram.json') as json_file:
+    def __init__(self, script_dir):
+        with open(script_dir + '/api_telegram.json') as json_file:
             data = json.load(json_file)
 
             self.token = data['token']
@@ -12,13 +15,12 @@ class Telegram:
             self.tb = telebot.TeleBot(self.token)
 
     def enviar_mensajes_a_telegram(self):
-        self.tb.send_message(self.ch_id, "Ya es hora de ponerse\
-		con el TFG, escorias")
+        self.tb.send_message(self.ch_id, "Puto Luis termina con tu TFG de una vez")
 
 
 
 if __name__ == '__main__':
-	teleg = Telegram()
-	while(1):
-		teleg.enviar_mensajes_a_telegram()
-		time.sleep(600) #10 minutos
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+	
+        teleg = Telegram(script_dir)
+        teleg.enviar_mensajes_a_telegram()
